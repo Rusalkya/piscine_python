46a9f137512047ed03c22407bcda8e7238faeb3f
clfouger <claire.fougeron27@gmail.com>
2026-06-15 09:50:00 +0200

    add ex02

--- 01/ex02/load_image.py
+++ 01/ex02/load_image.py
@@ -1,13 +1,19 @@
 import numpy as np
 from PIL import Image
 
-def ft_load(path: str): 
 
-    img = Image.open(ex02/animal.jpeg)
+def ft_load(path: str):
+
+    if not path.endswith((".jpg", ".jpeg")):
+        raise TypeError("Image's format must be a .jpg or .jpeg.")
+
+    try:
+        img = Image.open(path)
+    except Exception:
+        raise FileNotFoundError("Image doesn't exist.")
+
     array = np.array(img)
 
-    print(array.shape)
+    print(f"The shape of image is: {array.shape}")
 
-    if not path.endwith(".jpg") or not path.endswith(".jpeg"):
-        raise TypeError("image's format must be jpg or jpeg")
-    
+    return array
